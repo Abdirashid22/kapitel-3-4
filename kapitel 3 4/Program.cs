ed72308eaa80967bda6352d8780c0d81c5523388
absa07002 <absa07002@DESKTOP-TAGHD5A>
2024-12-17 18:22:40 +0100

    Add project files.

---
+++ kapitel 3 4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.Write("Ange låtens längd i minuter: ");
+        int minuter = int.Parse(Console.ReadLine());
+
+        Console.Write("Ange låtens längd i sekunder: ");
+        int sekunder = int.Parse(Console.ReadLine());
+
+
+        int totalSekunder = minuter * 60 + sekunder;
+
+
+        int minGräns = 2 * 60 + 45; // 2 minuter och 45 sekunder
+        int maxGräns = 4 * 60 + 20; // 4 minuter och 20 sekunder
+
+
+        if (totalSekunder >= minGräns && totalSekunder <= maxGräns)
+        {
+            Console.WriteLine("Låten får spelas på radiostationen.");
+        }
+        else
+        {
+            Console.WriteLine("Låten får inte spelas på radiostationen.");
+        }
+    }
+}
+
